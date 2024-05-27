0badb8ae41c231d79db5853e324985b95796c617
Rodrigo <rodrigopg@playkids.com>
2024-05-27 15:45:48 -0300

    fix10

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-var epubPath;
 var showConsoleLibName = false;
 var showNativeLog = false;
 var showNativeMenu = false;
@@ -10,21 +9,6 @@ window.addEventListener("load", function (event) {
 
 function initScripts() {
   showConsoleLibName = false;
-
-  let params = new URL(document.location).searchParams;
-  epubPath = params.get("epub");
-
-  const renditionsMobile = document.createElement("script");
-  const renditionsDesktop = document.createElement("script");
-  const settings = document.createElement("script");
-
-  renditionsMobile.src = epubPath + "/renditions-mobile.js";
-  renditionsDesktop.src = epubPath + "/renditions-desktop.js";
-  settings.src = epubPath + "/settings.js";
-
-  document.head.appendChild(settings);
-  document.head.appendChild(renditionsMobile);
-  document.head.appendChild(renditionsDesktop);
 
   $("#favoriteBtn").click(function (e) {
     e.preventDefault();
